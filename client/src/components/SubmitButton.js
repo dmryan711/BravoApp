6832f04e7f3e927b0fa178025e7a3cb9f3ce0c4e
Devon <dmryan711@users.noreply.github.com>
2021-01-10 23:12:38 -0500

    Submitbuttonlogic (#14)
    * Submit button logic done, still need to add reports to global state
    
    * partially done
    
    * added report type to global state, made report bar disable when prereqs not selected, finalized submit button disabled logic

--- client/src/components/SubmitButton.js
+++ client/src/components/SubmitButton.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, {Component} from "react";
 import Button from "react-bootstrap/Button";
-// import "bootstrap/dist/css/bootstrap.min.css";
-export default function SubmitButton() {
+import { connect } from 'react-redux';
+import { Container } from "react-bootstrap";
+class  SubmitButton extends Component {
+    handleClick = () => {
+        if(this.props.isFileUploaded && this.props.facets && this.props.facetSelection){
+            console.log("File is uploaded and there are facets,facet selection has been made, and report is selected");
+        }
+    }
+    render(){
+        return (
+            <Container>
+                {this.props.isFileUploaded && this.props.facets && this.props.facetSelection ? 
+                    <Button onClick = {this.handleClick}variant="primary" size="lg" block>
+                        Submit Report
+                    </Button>
+                    :
+                    <Button variant="secondary" size="lg" block disabled>
+                        Submit Report
+                    </Button>
+                }
 
-  return (
-    <Button variant="primary" size="lg" block>
-    Submit Report
-  </Button>
-  );
+            </Container>
+            
+        );
+    }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        isFileUploaded: state.fileUpload.fileUploadState.isFileUploaded,
+        facets: state.fileUpload.facetSelectionState.facets,
+        facetSelection: state.facetSelection.facetSelection.facet
+    };
+};
+
+export default connect(mapStateToProps)(SubmitButton);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import fileUploadReducer from "./fileUploadReducer";
 import facetSelectionReducer from "./facetSelectionReducer";
+import reportStateReducer from "./reportStateReducer";
 export default combineReducers({
   fileUpload : fileUploadReducer,
-  facetSelection:facetSelectionReducer
+  facetSelection:facetSelectionReducer,
+  report:reportStateReducer
 
 });
